68b93d131b83480b2e541ed40202198114c1d94f
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2020-10-31 14:57:25 +0000

    added gitignore

--- server.js
+++ server.js
@@ -1,22 +1,61 @@
 // server.js
 // This is where your node app starts
 
-//load the 'express' module which makes writing webservers easy
+//load the 'express' module which makes writing web servers easy
 const express = require("express");
 const app = express();
+const lodash = require("lodash");
+const cors = require("cors");
+const port = 3001;
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
-
+app.use(cors());
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send(
+    "Hadiyah's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
 });
 
 //START OF YOUR CODE...
+app.get("/", function (request, response) {
+  response.send(
+    "Welcome to Hadiyah's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
+});
+
+//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.json(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes));
+});
+app.get("/quotes/lodash-random", function (request, response) {
+  response.send(lodash.sample(quotes));
+});
+
+//IMPLEMENTING SEARCH
+app.get("/quotes/search", function (request, response) {
+  let searchValue = request.query.term;
+  let searchedQuote = quotes.filter(
+    (quoteObject) =>
+      quoteObject.quote.toLowerCase().includes(searchValue.toLowerCase()) ||
+      quoteObject.author.toLowerCase().includes(searchValue.toLowerCase())
+  );
+  response.json(searchedQuote);
+});
+
+//ECHO PARAMETER
+app.get("/echo", function (request, response) {
+  let searchValue = request.query.word;
+  response.send(searchValue);
+});
 
 //...END OF YOUR CODE
 
@@ -29,6 +68,6 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+const listener = app.listen(port)
+  console.log("Your app is listening on port");
+
